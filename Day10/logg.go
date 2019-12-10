acb20b4fa55072878f4b349a76f3436acbcb8ec2
Teis Lindemark <teli@dips.no>
2019-12-10 10:03:09 +0100

    Fixed that the list of use did not get in the same order. So could not use modulo to decide when parseing

--- Day10/logg.go
+++ Day10/logg.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -14,20 +15,33 @@ func FloatToString(input_num float64) string {
 	return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
 
+func GetType(input string) string {
+	if strings.Contains(input, "toalettpapir") {
+		return "toalettpapir"
+	}
+	if strings.Contains(input, "sjampo") {
+		return "sjampo"
+	}
+	if strings.Contains(input, "tannkrem") {
+		return "tannkrem"
+	}
+	return "undefined"
+}
+
 func main() {
-	var monthMap map[string]int = make(map[string]int)
-	monthMap["Jan"] = 1
-	monthMap["Feb"] = 2
-	monthMap["Mar"] = 3
-	monthMap["Apr"] = 4
-	monthMap["May"] = 5
-	monthMap["Jun"] = 6
-	monthMap["Jul"] = 7
-	monthMap["Aug"] = 8
-	monthMap["Sep"] = 9
-	monthMap["Oct"] = 10
-	monthMap["Nov"] = 11
-	monthMap["Dec"] = 12
+	var monthMap map[string]string = make(map[string]string)
+	monthMap["Jan"] = "01"
+	monthMap["Feb"] = "02"
+	monthMap["Mar"] = "03"
+	monthMap["Apr"] = "04"
+	monthMap["May"] = "05"
+	monthMap["Jun"] = "06"
+	monthMap["Jul"] = "07"
+	monthMap["Aug"] = "08"
+	monthMap["Sep"] = "09"
+	monthMap["Oct"] = "10"
+	monthMap["Nov"] = "11"
+	monthMap["Dec"] = "12"
 
 	file, fileError := os.Open("logg.txt")
 	if fileError != nil {
@@ -45,50 +59,48 @@ func main() {
 	var numToiletRolls float64 = 0
 	var usedShampooSunday float64 = 0
 	var usedToiletPapirWednesday float64 = 0
-	var weekDay time.Weekday
 
+	var weekDay time.Weekday
 	var index int = 0
 	for scanner.Scan() {
 		if index%4 == 0 {
 			dateInput := scanner.Text()
-			date := fmt.Sprintf("2018 %s %s 00 00", strconv.Itoa(monthMap[dateInput[0:3]]), dateInput[4:6])
+			date := fmt.Sprintf("2018 %s %s 00 00", monthMap[dateInput[0:3]], dateInput[4:6])
 			parsedDate, dateErr := time.Parse("2006 01 02 15 04", date)
 			if dateErr != nil {
-				fmt.Println("Error parseing date")
+				fmt.Println("Error parseing date", dateErr)
 			}
 			weekDay = parsedDate.Weekday()
-			fmt.Println(weekDay)
 		} else {
-			if index%4 == 1 {
-				// Tannkrem
-				i, _ := strconv.ParseFloat(re.FindString(scanner.Text()), 32)
-				numTubesToothpaste += (i / 125)
-			}
-			if index%4 == 2 {
-				// Sjampoo
-				i, _ := strconv.ParseFloat(re.FindString(scanner.Text()), 32)
-				numShampooBottles += (i / 300.0)
-				if weekDay == time.Sunday {
-					usedShampooSunday += i
-				}
-			}
-			if index%4 == 3 {
+			input := scanner.Text()
+			inputType := GetType(input)
+			i, _ := strconv.ParseFloat(re.FindString(scanner.Text()), 32)
+			//fmt.Println(inputType + " => " + input + " => " + FloatToString(i))
+			if inputType == "toalettpapir" {
 				// Toalettpapir
-				i, _ := strconv.ParseFloat(re.FindString(scanner.Text()), 32)
 				numToiletRolls += (i / 25)
 				if weekDay == time.Wednesday {
 					usedToiletPapirWednesday += i
 				}
 			}
+			if inputType == "sjampo" {
+				numShampooBottles += (i / 300.0)
+				if weekDay == time.Sunday {
+					usedShampooSunday += i
+				}
+			}
+			if inputType == "tannkrem" {
+				numTubesToothpaste += (i / 125)
+			}
 		}
 		index += 1
 	}
 
-	fmt.Println(math.Round(numTubesToothpaste))
-	fmt.Println(math.Round(numShampooBottles))
-	fmt.Println(math.Round(numToiletRolls))
+	fmt.Println(math.Ceil(numTubesToothpaste))
+	fmt.Println(math.Ceil(numShampooBottles))
+	fmt.Println(math.Ceil(numToiletRolls))
 	fmt.Println(usedShampooSunday)
 	fmt.Println(usedToiletPapirWednesday)
 
-	fmt.Println(math.Round(numTubesToothpaste) * math.Round(numShampooBottles) * math.Round(numToiletRolls) * usedShampooSunday * usedToiletPapirWednesday)
+	fmt.Println(math.Ceil(numTubesToothpaste) * math.Ceil(numShampooBottles) * math.Ceil(numToiletRolls) * usedShampooSunday * usedToiletPapirWednesday)
 }
